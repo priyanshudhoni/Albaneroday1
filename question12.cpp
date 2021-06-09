3f0ac95360eb9e73e354855ce7d80334a6a66de3
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-09 15:16:21 +0530

    Update question12.cpp

--- question12.cpp
+++ question12.cpp
@@ -1,22 +1,25 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 int main()
 {
     string a;
     cin>>a;
     sort(a.begin(),a.end());
-    char j;int k,p,f=0;
+    int j;int k,p,f=0;
     string h=a;
-   
+   array<char,26>b;
+   for(j=0;j<26;j++)
+   b[j]=97+j;
     while(f<a.length())
     {
-        for(j='a';j<='z';j++)
+        for(j=0;j<26;j++)
         {
             for(k=0;k<a.length();k++)
             {
-                if(a[k]==j)
+                if(a[k]==b[j])
                 {
-                    cout<<j;
+                    cout<<b[j];
                     a[k]=-1;
                     f++;
                     break;
@@ -26,8 +29,7 @@ int main()
             if(f==a.length())
             break;
         }
-         for(p=a.length()-1;p>=0;p--)
-            h=h+a[p];
+         reverse(b.begin(),b.end());
         if(f==a.length())
         break;
     }
