feae0f55fed99d984151cc9dbd0d4366567b0fa3
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-11 10:48:23 +0530

    Update question33.cpp

--- question33.cpp
+++ question33.cpp
@@ -16,6 +16,7 @@ int main()
     
     for(j=0;j<a;j++)
     {
+        transform(s[j].begin(),s[j].end(),s[j].begin(),::tolower);
         if(s[j][0]>=97 && s[j][0]<=122)
         s[j][0]=s[j][0]-32;
     }
