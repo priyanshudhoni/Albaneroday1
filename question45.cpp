b41754a397c0c8c72a1cd2c6c4f1a4ef22ed2455
priyanshudhoni <59824018+priyanshudhoni@users.noreply.github.com>
2021-06-12 11:10:32 +0530

    Create question45.cpp

---
+++ question45.cpp
@@ -0,0 +1,66 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *ptr;
+}*head;
+
+void first()
+{
+    struct node *p=(struct node *)malloc(sizeof(struct node));
+    cout<<"Enter a value"<<"\n";
+    cin>>p->data;
+    p->ptr=NULL;
+    head=p;
+}
+void insert()
+{
+    struct node *p=(struct node *)malloc(sizeof(struct node));
+    cout<<"Enter a  number"<<"\n";
+    cin>>p->data;
+    p->ptr=NULL;
+    struct node *po;
+    
+    po=head;
+    while(po->ptr!=NULL)
+    {
+        po=po->ptr;
+    }
+    po->ptr=p;
+}
+
+void operation()
+{
+    struct node *p=head;  
+    while(p!=NULL && p->ptr!=NULL)
+    {
+        if(p->data==p->ptr->data)
+        {
+            struct node *i=p->ptr->ptr;
+            p->ptr=i;
+        }
+        else
+        p=p->ptr;
+    }
+
+    struct node *r=head;
+    while(r!=NULL)
+    {
+        cout<<r->data<<" ";
+        r=r->ptr;
+    }
+    
+}
+int main()
+{
+        cout<<"Enter the number of elements"<<"\n";
+        int a;
+        cin>>a;
+        first();
+        int j;
+        for(j=0;j<a-1;j++)
+        insert();
+        operation();
+}
